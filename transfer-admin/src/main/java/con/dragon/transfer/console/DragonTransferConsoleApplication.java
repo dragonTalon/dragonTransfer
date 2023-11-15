9a2919bb0b372767dedf1446bfeeeb8987b2acee
yifan.jian <yifan.jian@tcl.com>
2023-11-15 11:16:29 +0800

    init

---
+++ transfer-admin/src/main/java/con/dragon/transfer/console/DragonTransferConsoleApplication.java
@@ -0,0 +1,17 @@
+package con.dragon.transfer.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Title:main
+ * @Author dragon
+ * @Description
+ * @Date 2023/11/15 11:00
+ **/
+@SpringBootApplication
+public class DragonTransferConsoleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DragonTransferConsoleApplication.class, args);
+    }
+}
---
+++ transfer-channel/src/main/java/com/dragon/transfer/channel/DragonTransferChannelApplication.java
@@ -0,0 +1,17 @@
+package com.dragon.transfer.channel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Title :main
+ * @Author dragon
+ * @Description
+ * @Date 2023/11/15 10:58
+ **/
+@SpringBootApplication
+public class DragonTransferChannelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DragonTransferChannelApplication.class, args);
+    }
+}
